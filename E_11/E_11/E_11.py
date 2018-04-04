8417fbd6524895e93a6ac82461d13569833afa1b
Gauravism2017 <gauravism@outlook.com>
2018-04-04 06:34:37 +0530

    Add files via upload

---
+++ E_11/E_11/E_11.py
@@ -0,0 +1,16 @@
+array = []
+
+arr2d = [[j for j in input().split(' ')] for i in range(20)] 
+       
+for y in range(20):
+    for x in range(20):
+        if x < 17:
+            array.append(int(arr2d[y][x])*int(arr2d[y][x+1])*int(arr2d[y][x+2])*int(arr2d[y][x+3]))
+        if y < 17:
+            array.append(int(arr2d[y][x])*int(arr2d[y+1][x])*int(arr2d[y+2][x])*int(arr2d[y+3][x]))
+        if x >= 3 and y < 17:
+            array.append(int(arr2d[y][x])*int(arr2d[y+1][x-1])*int(arr2d[y+2][x-2])*int(arr2d[y+3][x-3]))
+        if x < 17 and y < 17:
+            array.append(int(arr2d[y][x])*int(arr2d[y+1][x+1])*int(arr2d[y+2][x+2])*int(arr2d[y+3][x+3]))
+            
+print(max(array))
