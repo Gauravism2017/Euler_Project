1359b23c8ad02a6774337f03a0765336d3a12792
Gauravism2017 <gauravism@outlook.com>
2018-04-05 07:45:42 +0530

    Add files via upload

---
+++ E14/E14/E14.py
@@ -0,0 +1,20 @@
+import time
+start = time.time()
+memo = [0, 1] + [0] * 5000000
+result = [0, 1] + [1] * 5000000
+
+def collatzLength(n):
+    if n <= 5000000:
+        res = memo[n]
+        if not res: res = memo[n] = 1 + collatzLength(3*n + 1 if n % 2 else n // 2)
+    else:
+        res = 1 + collatzLength(3*n + 1 if n % 2 else n // 2)
+    return res
+
+for n in range(2, 5000001):
+    l = collatzLength(n)
+    result[n] = n if l >= memo[result[n - 1]] else result[n - 1]
+print(time.time()-start)
+
+for _ in range(int(input())):
+    print (result[int(input())])
